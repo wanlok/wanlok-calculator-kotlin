3d5b0e099f84e25f3382d5b871ffab040c4b2aba
Robert Wan <wantunglok@gmail.com>
2024-02-23 12:55:29 +1100

    convert parser to support big decimals

--- app/src/main/java/com/wanlok/calculator/Utils.kt
+++ app/src/main/java/com/wanlok/calculator/Utils.kt
@@ -1,6 +1,7 @@
 package com.wanlok.calculator
 
 import android.content.Context
+import android.util.Log
 import android.util.TypedValue
 import android.util.TypedValue.COMPLEX_UNIT_DIP
 import java.math.BigDecimal
@@ -20,35 +21,61 @@ object Utils {
         return if (x.isEmpty()) "" else BigDecimal(x).stripTrailingZeros().toPlainString()
     }
 
+    fun plus(x: BigDecimal, y: BigDecimal): BigDecimal {
+        return x.plus(y)
+    }
+
     fun plus(x: String, y: String): String {
-        return stripTrailingZeros(BigDecimal(x).plus(BigDecimal(y)).toPlainString())
+        return stripTrailingZeros(plus(BigDecimal(x), BigDecimal(y)).toPlainString())
+    }
+
+    fun minus(x: BigDecimal, y: BigDecimal): BigDecimal {
+        return x.minus(y)
     }
 
     fun minus(x: String, y: String): String {
-        return stripTrailingZeros(BigDecimal(x).minus(BigDecimal(y)).toPlainString())
+        return stripTrailingZeros(minus(BigDecimal(x), BigDecimal(y)).toPlainString())
+    }
+
+    fun multiply(x: BigDecimal, y: BigDecimal): BigDecimal {
+        return x.multiply(y)
     }
 
     fun multiply(x: String, y: String): String {
-        return stripTrailingZeros(BigDecimal(x).multiply(BigDecimal(y)).toPlainString())
+        return stripTrailingZeros(multiply(BigDecimal(x), BigDecimal(y)).toPlainString())
+    }
+
+    fun divide(x: BigDecimal, y: BigDecimal): BigDecimal {
+        return x.divide(y, 2, RoundingMode.HALF_UP)
     }
 
     fun divide(x: String, y: String): String {
-        return stripTrailingZeros(BigDecimal(x).divide(BigDecimal(y), 2, RoundingMode.HALF_UP).toPlainString())
+        return stripTrailingZeros(divide(BigDecimal(x), BigDecimal(y)).toPlainString())
+    }
+
+    fun power(x: BigDecimal, power: BigDecimal): BigDecimal {
+        return x.pow(power.toInt())
     }
 
     fun power(x: String, power: String): String {
-        return stripTrailingZeros(BigDecimal(x).pow(power.toInt()).toPlainString())
+        return stripTrailingZeros(power(BigDecimal(x), BigDecimal(power)).toPlainString())
+    }
+
+    fun squareroot(x: BigDecimal): BigDecimal {
+        return BigDecimal(sqrt(x.toDouble()))
     }
 
     fun squareroot(x: String): String {
-        return stripTrailingZeros(sqrt(x.toDouble()).toString())
+        return stripTrailingZeros(squareroot(BigDecimal(x)).toPlainString())
     }
 
     fun isZero(x: String): Boolean {
         return BigDecimal(x) != BigDecimal.ZERO
     }
 
-    fun eval(str: String): Double {
+    fun eval(str: String): BigDecimal {
+        Log.d("ROBERT", "INPUT FORMULA: " + str)
+
         return object : Any() {
             var pos = -1
             var ch = 0
@@ -65,7 +92,7 @@ object Utils {
                 return false
             }
 
-            fun parse(): Double {
+            fun parse(): BigDecimal {
                 nextChar()
                 val x = parseExpression()
                 if (pos < str.length) throw RuntimeException("Unexpected: " + ch.toChar())
@@ -78,37 +105,55 @@ object Utils {
             // factor = `+` factor | `-` factor | `(` expression `)` | number
             //        | functionName `(` expression `)` | functionName factor
             //        | factor `^` factor
-            fun parseExpression(): Double {
+            fun parseExpression(): BigDecimal {
                 var x = parseTerm()
                 while (true) {
-                    if (eat('+'.code)) x += parseTerm() // addition
-                    else if (eat('-'.code)) x -= parseTerm() // subtraction
-                    else return x
+                    if (eat('+'.code)) {
+                        x = plus(x, parseTerm()) // addition
+                    } else if (eat('-'.code)) {
+                        x = minus(x, parseTerm()) // subtraction
+                    } else {
+                        return x
+                    }
                 }
             }
 
-            fun parseTerm(): Double {
+            fun parseTerm(): BigDecimal {
                 var x = parseFactor()
                 while (true) {
-                    if (eat('*'.code)) x *= parseFactor() // multiplication
-                    else if (eat('/'.code)) x /= parseFactor() // division
-                    else return x
+                    if (eat('*'.code)) {
+                        x = multiply(x, parseFactor()) // multiplication
+                    } else if (eat('/'.code)) {
+                        x = divide(x, parseFactor()) // division
+                    } else {
+                        return x
+                    }
                 }
             }
 
-            fun parseFactor(): Double {
-                if (eat('+'.code)) return +parseFactor() // unary plus
-                if (eat('-'.code)) return -parseFactor() // unary minus
-                var x: Double
+            fun parseFactor(): BigDecimal {
+                if (eat('+'.code)) {
+                    return BigDecimal("+" + parseFactor().toPlainString()) // unary plus
+                }
+                if (eat('-'.code)) {
+                    return BigDecimal("-" + parseFactor().toPlainString()) // unary minus
+                }
+                var x: BigDecimal
                 val startPos = pos
                 if (eat('('.code)) { // parentheses
                     x = parseExpression()
-                    if (!eat(')'.code)) throw RuntimeException("Missing ')'")
+                    if (!eat(')'.code)) {
+                        throw RuntimeException("Missing ')'")
+                    }
                 } else if (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) { // numbers
-                    while (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) nextChar()
-                    x = str.substring(startPos, pos).toDouble()
+                    while (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) {
+                        nextChar()
+                    }
+                    x = BigDecimal(str.substring(startPos, pos))
                 } else if (ch >= 'a'.code && ch <= 'z'.code) { // functions
-                    while (ch >= 'a'.code && ch <= 'z'.code) nextChar()
+                    while (ch >= 'a'.code && ch <= 'z'.code) {
+                        nextChar()
+                    }
                     val func = str.substring(startPos, pos)
                     if (eat('('.code)) {
                         x = parseExpression()
@@ -118,20 +163,24 @@ object Utils {
                     }
                     x =
                         if (func == "sqrt") {
-                            Math.sqrt(x)
-                        } else if (func == "sin") Math.sin(
-                            Math.toRadians(
-                                x
-                            )
-                        ) else if (func == "cos") Math.cos(
-                            Math.toRadians(x)
-                        ) else if (func == "tan") Math.tan(Math.toRadians(x)) else throw RuntimeException(
+                            squareroot(x)
+                        }
+//                        else if (func == "sin") Math.sin(
+//                            Math.toRadians(
+//                                x
+//                            )
+//                        ) else if (func == "cos") Math.cos(
+//                            Math.toRadians(x)
+//                        ) else if (func == "tan") Math.tan(Math.toRadians(x))
+                        else throw RuntimeException(
                             "Unknown function: $func"
                         )
                 } else {
                     throw RuntimeException("Unexpected: " + ch.toChar())
                 }
-                if (eat('^'.code)) x = Math.pow(x, parseFactor()) // exponentiation
+                if (eat('^'.code)) {
+                    x = power(x, parseFactor()) // exponentiation
+                }
                 return x
             }
         }.parse()
